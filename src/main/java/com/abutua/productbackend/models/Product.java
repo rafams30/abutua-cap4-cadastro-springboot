a82ba77ed45911cfa507234e5f0f494c7e5ca5a0
Rafael Martines <rafams_22@hotmail.com>
2024-09-17 21:50:51 -0300

    Alternado Class Product

--- src/main/java/com/abutua/productbackend/models/Product.java
+++ src/main/java/com/abutua/productbackend/models/Product.java
@@ -2,68 +2,31 @@ package com.abutua.productbackend.models;
 
 public class Product {
 
-    // Atributos
     private int id;
     private String name;
     private String description;
-    private int idCategory;
+    private Category category;
     private boolean promotion;
     private boolean newProduct;
     private double price;
 
-    // Métodos Construtores
+    public Product(int id, String name, String description, Category category, boolean promotion, boolean newProduct, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.promotion = promotion;
+        this.newProduct = newProduct;
+        this.price = price;
+    }
+
     public Product(int id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
 
-    public Product(int id, String name, String description, int idCategory, boolean promotion, boolean newProduct,
-            double price) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.idCategory = idCategory;
-        this.promotion = promotion;
-        this.newProduct = newProduct;
-        this.price = price;
-    }
-
     public Product() {
-
-    }
-
-    // Métodos
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getIdCategory() {
-        return idCategory;
-    }
-
-    public void setIdCategory(int idCategory) {
-        this.idCategory = idCategory;
-    }
-
-    public boolean isPromotion() {
-        return promotion;
-    }
-
-    public void setPromotion(boolean promotion) {
-        this.promotion = promotion;
-    }
-
-    public boolean isNewProduct() {
-        return newProduct;
-    }
-
-    public void setNewProduct(boolean newProduct) {
-        this.newProduct = newProduct;
     }
 
     public int getId() {
@@ -88,5 +51,37 @@ public class Product {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public boolean isPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(boolean promotion) {
+        this.promotion = promotion;
+    }
+
+    public boolean isNewProduct() {
+        return newProduct;
+    }
+
+    public void setNewProduct(boolean newProduct) {
+        this.newProduct = newProduct;
     }
 }
